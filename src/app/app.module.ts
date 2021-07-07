a5200a973615a2476c61a367388e3a3ce23d0c9d
Marcos Paulo <marcos_pa32@hotmail.com>
2021-07-07 09:59:46 -0300

    projeto angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { PainelComponent } from './painel/painel.component';
+import { TentativasComponent } from './tentativas/tentativas.component';
+import { ProgressoComponent } from './progresso/progresso.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopComponent,
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/top/top.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-topo',
+    templateUrl: './top.component.html',
+    styleUrls: ['./top.component.css']
+
+})
+
+export class TopComponent{
+
+}
